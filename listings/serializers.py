f9d485b06bc6f3515ab5ca204185ff8862a7abdf
Malik Rasaq <malikrasaq@gmail.com>
2023-08-14 17:44:05 +0100

    Rating serializer

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Listing
 
 
 class ListingSerializer(ModelSerializer):
+	rating = serializers.DecimalField(max_digits=3, decimal_places=1)
 	class Meta:
 		model = Listing
 		fields = "__all__"
