13bf9691c14e0565d6f051f045b93899b08f9223
kingsli <kingoyelabi@gmail.com>
2023-08-14 20:19:48 +0100

    update allowed hosts

--- host/settings.py
+++ host/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
