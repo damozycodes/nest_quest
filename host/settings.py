7809a9c2f73fccec9cb8ce89d61521d8e4e37b8f
kingsli <kingoyelabi@gmail.com>
2023-08-16 12:25:36 +0100

    deployment script

--- host/settings.py
+++ host/settings.py
@@ -157,3 +157,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = "mails"
